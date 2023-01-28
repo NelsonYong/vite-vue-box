bb1dd581e6265838cf4f46b6681b6706e52f669b
YongGit <1013588891@qq.com>
2023-01-28 11:16:59 +0800

    feat: auto import vue-hooks-plus

--- config/constant.ts
+++ config/constant.ts
@@ -7,17 +7,17 @@
 export const APP_TITLE = 'App Name'
 
 // server port
-export const VITE_PORT = 3000
+export const VITE_PORT = 3172
 
 // prefix
 export const API_PREFIX = '/api'
 
 // serve
-export const API_TARGET_URL = 'http://localhost:3000'
+export const API_TARGET_URL = 'http://localhost:3172'
 
 // mock
 export const MOCK_API_BASE_URL = '/mock/api'
-export const MOCK_API_TARGET_URL = 'http://localhost:3000'
+export const MOCK_API_TARGET_URL = 'http://localhost:3172'
 
 // code gz
 export const COMPRESSION = true
--- config/vite/plugins/autoImport.ts
+++ config/vite/plugins/autoImport.ts
@@ -1,6 +1,6 @@
 /**
  * @name AutoImportDeps
- * @description 按需加载，自动引入依赖
+ * @description auto-import lib
  */
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueHooksPlusResolver } from '@vue-hooks-plus/resolvers'
